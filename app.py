6b2a1eb5431e75af829109a250b76244ebd0acd4
Francesco Margiotta <francesco.margiotta@teratecnologies.es>
2022-09-07 09:24:51 +0200

    aggiunti file

---
+++ app.py
@@ -0,0 +1,51 @@
+import os
+
+import boto3
+from flask import Flask, jsonify, make_response, request
+
+app = Flask(__name__)
+
+
+dynamodb_client = boto3.client('dynamodb')
+
+if os.environ.get('IS_OFFLINE'):
+    dynamodb_client = boto3.client(
+        'dynamodb', region_name='localhost', endpoint_url='http://localhost:8000'
+    )
+
+
+USERS_TABLE = os.environ['USERS_TABLE']
+
+
+@app.route('/getUserById/<string:user_id>')
+def get_user(user_id):
+    result = dynamodb_client.get_item(
+        TableName=USERS_TABLE, Key={'userId': {'S': user_id}}
+    )
+    item = result.get('Item')
+    if not item:
+        return jsonify({'error': 'Utente con ID {} non trovato '.format(user_id)}), 404
+
+    return jsonify(
+        {'userId': item.get('userId').get('S'), 'name': item.get('name').get('S')}
+    )
+
+
+@app.route('/createUser', methods=['POST'])
+def create_user():
+    user_id = request.json.get('userId')
+    name = request.json.get('username')
+    password = request.json.get('password')
+    if not user_id or not name or not password:
+        return jsonify({'Errore': '"userId", "username" e "password" richiesti'}), 400
+
+    dynamodb_client.put_item(
+        TableName=USERS_TABLE, Item={'userId': {'S': user_id}, 'username': {'S': name}, 'password':{'S':password}}
+    )
+
+    return jsonify({'userId': user_id}),201
+
+
+@app.errorhandler(404)
+def resource_not_found(e):
+    return make_response(jsonify(error='Endpoint non trovato. Rotte consentite: getUserById, createUser'), 404)
